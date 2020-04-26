87fd02fb787126c054e2915b9574c564e0efba07
ErasylAbenov <abenov.erasyl@gmail.com>
2020-04-26 15:47:13 +0600

    online shop start

---
+++ Shop/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category{ CategoryId=1, CategoryName="Fruit pies", Description="All-fruit pies"},
+                new Category{ CategoryId=1, CategoryName="Cheese pies", Description="Cheesy all the way"},
+                new Category{ CategoryId=1, CategoryName="Season pies", Description="Get in the mood for a season pie"}
+            };
+    }
+}
